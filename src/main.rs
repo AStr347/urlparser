7a7c6e08642b11ac8689d8b86fbd6c88d2b4b594
AStr347 <anton3000str@mail.ru>
2024-08-23 13:18:47 +0300

    fix(proj): update dockerfile; fix(proj): cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,6 @@ use playwright::Playwright;
 mod handlers;
 use crate::handlers::*;
 
-// #[tokio::main]
-// async fn main() -> Result<(), playwright::Error> {
-//     let playwright = Playwright::initialize().await?;
-//     playwright.prepare()?; // Install browsers
-//     let chromium = playwright.chromium();
-//     let browser = chromium.launcher().headless(true).launch().await?;
-//     let context = browser.context_builder().build().await?;
-//     let page = context.new_page().await?;
-//     page.goto_builder("https://example.com/").goto().await?;
-
-//     let content = page.content().await?;
-//     println!("{content}");
-
-//     Ok(())
-// }
-
-
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let pw = Playwright::initialize().await.unwrap();
